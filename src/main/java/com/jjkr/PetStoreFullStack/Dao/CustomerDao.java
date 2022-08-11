7401f740e5ef661f44ecdc8813b3364e336236e5
KDrake80 <kevindrake80@gmail.com>
2022-08-11 15:03:58 -0600

    Added Updates
    Updated Maven in IntelliJ, and React in VS. Added Rest and Database

---
+++ src/main/java/com/jjkr/PetStoreFullStack/Dao/CustomerDao.java
@@ -0,0 +1,9 @@
+package com.jjkr.PetStoreFullStack.Dao;
+
+import com.jjkr.PetStoreFullStack.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDao extends JpaRepository<Customer, Integer> {
+}
